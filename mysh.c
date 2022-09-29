c53f5cff10d619f9ec01b26985d1e3aeb36a800f
Patrick <patrickburns2557@gmail.com>
2022-09-29 00:34:41 -0500

    ps1 command done

--- mysh.c
+++ mysh.c
@@ -12,18 +12,21 @@ Program: My Shell
 #include "stdbool.h"
 
 /* Function Prototypes */
-void echo(char userInput[1024]);
-void ps1(char userInput[1024]);
-void cat(char userInput[1024]);
-void cp(char userInput[1024]);
-void rm(char userInput[1024]);
-void mkdir(char userInput[1024]);
-void rmdir(char userInput[1024]);
+void command_echo(char userInput[1024]);
+void command_ps1(char userInput[1024]);
+void command_cat(char userInput[1024]);
+void command_cp(char userInput[1024]);
+void command_rm(char userInput[1024]);
+void command_mkdir(char userInput[1024]);
+void command_rmdir(char userInput[1024]);
+
+/* Global Variable */
+char ps1[1024] = "$";
+
 
 int main()
 {
 	int x = 0;
-	char ps1[] = "$";
 	char userInput[1024];
 	char command[128];
 	
@@ -34,16 +37,16 @@ int main()
 	{
 		printf("%s ", ps1);
 		fgets(userInput, 1024, stdin);
-		/*printf("%s\n", userInput);*/
+		
 
 
 		if (strncmp(userInput, "echo ", 5) == 0)
 		{
-			echo(userInput);
+			command_echo(userInput);
 		}
-		else if (strncmp(userInput, "ps1 ", 4) == 0)
+		else if ((strncmp(userInput, "ps1 ", 4) == 0) || (strncmp(userInput, "PS1 ", 4) == 0))
 		{
-			printf("command ps1 entered\n");
+			command_ps1(userInput);
 		}
 		else if (strncmp(userInput, "cat ", 4) == 0)
 		{
@@ -80,8 +83,8 @@ int main()
 	return 0;
 }
 
-
-void echo(char userInput[1024])
+/* Function handles the echo command */
+void command_echo(char userInput[1024])
 {
 	bool nArg = false; /* used to determine whether to apply -n */
 	char* token; /* used during tokenization */
@@ -133,32 +136,173 @@ void echo(char userInput[1024])
 	}	
 }
 
-void ps1(char userInput[1024])
+/* Function handles the PS1 command */
+void command_ps1(char userInput[1024])
 {
+	char* token; /* used during tokenization */
+	char tokens[128][256];
+	char newPs1[1024] = "";
+	int numTokens = 0;
+	int i = 0;  /* declared here since using -ansi disallows var declarations inside for loops */
+
+	token = strtok(userInput, " ");
+	while(token != NULL)
+	{
+		strcpy(tokens[numTokens++], token);
+		token = strtok(NULL," ");
+	}
+
+	/* Replace the newline character in the last token with a 0 */
+	for(i = 0;;i++)
+	{
+		if(tokens[numTokens-1][i] == '\n')
+		{
+			tokens[numTokens-1][i] = 0;
+			break;
+		}
+	}
+
+	/* concatenate each token to the newPs1 string */
+	for(i = 1; i<numTokens;i++)
+	{
+		strcat(newPs1, tokens[i]);
+		if(i < numTokens-1) /* don't add a space after the last string */
+		{
+			strcat(newPs1, " ");
+		}
+	}
+
+	/* set the new PS1 variable */
+	strcpy(ps1, newPs1);
 
 }
 
-void cat(char userInput[1024])
+/* Function handles the cat command */
+void command_cat(char userInput[1024])
 {
+	char* token;
+	char tokens[128][256];
+	int numTokens;
+	int i = 0;
 
+	token = strtok(userInput, " ");
+	while(token != NULL)
+	{
+		strcpy(tokens[numTokens++], token);
+		token = strtok(NULL," ");
+	}
+
+	/* Replace the newline character in the last token with a 0 */
+	for(i = 0;;i++)
+	{
+		if(tokens[numTokens-1][i] == '\n')
+		{
+			tokens[numTokens-1][i] = 0;
+			break;
+		}
+	}
 }
 
-void cp(char userInput[1024])
+/* Function handles the cp command */
+void command_cp(char userInput[1024])
 {
+	char* token;
+	char tokens[128][256];
+	int numTokens;
+	int i = 0;
 
+	token = strtok(userInput, " ");
+	while(token != NULL)
+	{
+		strcpy(tokens[numTokens++], token);
+		token = strtok(NULL," ");
+	}
+
+	/* Replace the newline character in the last token with a 0 */
+	for(i = 0;;i++)
+	{
+		if(tokens[numTokens-1][i] == '\n')
+		{
+			tokens[numTokens-1][i] = 0;
+			break;
+		}
+	}
 }
 
-void rm(char userInput[1024])
+/* Function handles the rm command */
+void command_rm(char userInput[1024])
 {
+	char* token;
+	char tokens[128][256];
+	int numTokens;
+	int i = 0;
 
+	token = strtok(userInput, " ");
+	while(token != NULL)
+	{
+		strcpy(tokens[numTokens++], token);
+		token = strtok(NULL," ");
+	}
+
+	/* Replace the newline character in the last token with a 0 */
+	for(i = 0;;i++)
+	{
+		if(tokens[numTokens-1][i] == '\n')
+		{
+			tokens[numTokens-1][i] = 0;
+			break;
+		}
+	}
 }
 
-void mkdir(char userInput[1024])
+/* Function handles the mkdir command */
+void command_mkdir(char userInput[1024])
 {
+	char* token;
+	char tokens[128][256];
+	int numTokens;
+	int i = 0;
 
+	token = strtok(userInput, " ");
+	while(token != NULL)
+	{
+		strcpy(tokens[numTokens++], token);
+		token = strtok(NULL," ");
+	}
+
+	/* Replace the newline character in the last token with a 0 */
+	for(i = 0;;i++)
+	{
+		if(tokens[numTokens-1][i] == '\n')
+		{
+			tokens[numTokens-1][i] = 0;
+			break;
+		}
+	}
 }
 
-void rmdir(char userInput[1024])
+/* Function handles the rmdir command */
+void command_rmdir(char userInput[1024])
 {
+	char* token;
+	char tokens[128][256];
+	int numTokens;
+	int i = 0;
 
+	token = strtok(userInput, " ");
+	while(token != NULL)
+	{
+		strcpy(tokens[numTokens++], token);
+		token = strtok(NULL," ");
+	}
+
+	/* Replace the newline character in the last token with a 0 */
+	for(i = 0;;i++)
+	{
+		if(tokens[numTokens-1][i] == '\n')
+		{
+			tokens[numTokens-1][i] = 0;
+			break;
+		}
+	}
 }
